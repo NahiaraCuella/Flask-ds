3d9bf41fcbe513d3df1f299ede896f953d33aea7
NahiaraCuella <nahiaracuella16@gmail.com>
2025-05-19 15:59:13 -0300

    use url_for, verificar si esta bien y me falta dos mas

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return f"""<p>Hello, World!</p>
+    <a href="{{ url_for('nombre') }}">Una ruta sencilla</a>
+"""
 
 @app.route("/nombre")
 def nombre():
